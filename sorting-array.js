d780603714152877e897b479561a9eaae4bc026d
aneesawan34 <aneesawan34@gmail.com>
2023-11-02 22:36:45 +0500

    sorting of array with for loop

---
+++ sorting-array.js
@@ -0,0 +1,12 @@
+let data = [40, 30, 12, 25];
+for(let i = 0; i < data.length;i++){
+    for (let j = 0; j < data.length; j++) {
+        const element = data[j];
+    if(data[j]>data[j+1]){
+        let temp = data[j];
+        data[j] = data[j+1];
+        data[j+1] = temp;
+    }
+    }
+}
+console.warn('data: ', data);
